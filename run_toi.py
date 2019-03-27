21815a29627cdb5b48e1e939e0e282657fe5dbe0
Dan F-M <foreman.mackey@gmail.com>
2019-03-27 14:45:27 -0500

    adding 'notebooks/0.1.0/toi-144.ipynb'

--- run_toi.py
+++ run_toi.py
@@ -3,8 +3,11 @@
 
 from __future__ import division, print_function
 
+import re
+import os
 import sys
 import nbformat
+import subprocess
 from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 
 __version__ = "0.1.0"
@@ -14,9 +17,14 @@ if len(sys.argv) < 2:
 
 toi_number = int(sys.argv[1])
 filename = "notebooks/{0}/toi-{1}.ipynb".format(__version__, toi_number)
+os.makedirs(os.path.dirname(filename), exist_ok=True)
 
 with open("template.ipynb", "r") as f:
-    txt = f.read().replace("{{{TOINUMBER}}}", "{0}".format(toi_number))
+    txt = f.read()
+    txt = txt.replace("{{{TOINUMBER}}}", "{0}".format(toi_number))
+    txt = txt.replace("{{{VERSIONNUMBER}}}", "{0}".format(__version__))
+    txt = re.sub(r"toi_num = [0-9]+", "toi_num = {0}".format(toi_number),
+                 txt)
 
 with open(filename, "w") as f:
     f.write(txt)
@@ -37,3 +45,7 @@ except CellExecutionError as e:
 finally:
     with open(filename, mode="wt") as f:
         nbformat.write(notebook, f)
+
+subprocess.check_call("git add {0}".format(filename), shell=True)
+subprocess.check_call("git commit -m \"adding '{0}'\"".format(filename),
+                      shell=True)

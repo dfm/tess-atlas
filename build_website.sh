0c23a0c00b016824232de0be4d9a32e5f9619b7f
Dan F-M <foreman.mackey@gmail.com>
2019-03-29 10:22:23 -0500

    removing html from master

---
+++ build_website.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm -rf build
+git clone . build
+cd build
+pwd
+make html
+git checkout --orphan gh-pages
+git rm --cached -rf .
+cp -R docs/* .
+cp docs/.nojekyll .
+git add -f CNAME css static index.html notebooks .nojekyll
+git commit -m "updating the site"
+git push -f origin gh-pages
+

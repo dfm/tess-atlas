ab9095361b1aab515b1b5f46a6bd4e73775692db
Dan F-M <foreman.mackey@gmail.com>
2019-08-13 10:57:13 -0400

    starting the paper

---
+++ paper/gen_links.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8")[
+    :-1
+]
+slug = "dfm/tess-atlas"
+with open("gitlinks.tex", "w") as f:
+    print(
+        r"\newcommand{\codelink}[1]{\href{https://github.com/%s/blob/%s/paper/figures/#1.ipynb}{\codeicon}\,\,}"
+        % (slug, hash),
+        file=f,
+    )
+    print(
+        r"\newcommand{\animlink}[1]{\href{https://github.com/%s/blob/%s/paper/figures/#1.gif}{\animicon}\,\,}"
+        % (slug, hash),
+        file=f,
+    )
+    print(
+        r"\newcommand{\prooflink}[1]{\href{https://github.com/%s/blob/%s/paper/proofs/#1.ipynb}{\raisebox{-0.1em}{\prooficon}}}"
+        % (slug, hash),
+        file=f,
+    )

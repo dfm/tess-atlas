23bc81ad48281ea099d3ec151256a7c5475f2039
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-09-09 18:10:14 +1000

    debug pyc3 error

--- run_toi.py
+++ run_toi.py
@@ -47,8 +47,8 @@ def create_toi_notebook_from_template_notebook(
         txt = txt.replace("{{{VERSIONNUMBER}}}", f"'{version}'")
         txt = re.sub(r"toi_num = [0-9]+", f"toi_num = {toi_number}", txt)
         if quickrun:
-            txt = re.sub(r"TUNE = [0-9]+", f"TUNE = {20}", txt)
-            txt = re.sub(r"DRAWS = [0-9]+", f"DRAWS = {20}", txt)
+            txt = re.sub(r"TUNE = [0-9]+", f"TUNE = {200}", txt)
+            txt = re.sub(r"DRAWS = [0-9]+", f"DRAWS = {200}", txt)
             txt = re.sub(r"CHAINS = [0-9]+", f"CHAINS = {1}", txt)
 
     with open(notebook_filename, "w") as f:
@@ -87,7 +87,7 @@ def execute_toi_notebook(notebook_filename, version=__version__):
         msg += e.traceback
         msg += f"removing: {notebook_filename}\n\n"
         print(msg)
-        os.remove(notebook_filename)
+        # os.remove(notebook_filename)
         success = False
     else:
         with open(notebook_filename, mode="wt") as f:

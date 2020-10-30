a0a765833d55746e38d12377ee582da6852786de
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-10-30 17:08:29 +1100

    refactoring

--- src/tess_atlas/utils.py
+++ src/tess_atlas/utils.py
@@ -4,6 +4,7 @@ import logging
 import multiprocessing as mp
 import warnings
 
+import matplotlib.pyplot as plt
 import nbformat
 from IPython import get_ipython
 from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
@@ -52,7 +53,19 @@ def notebook_initalisations():
         "exoplanet",
         "matplotlib",
         "urllib3",
+        "arviz",
+        "astropy",
     ]:
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.ERROR)
     logging.getLogger().setLevel(logging.INFO)
+
+    plt.style.use("default")
+    plt.rcParams["savefig.dpi"] = 100
+    plt.rcParams["figure.dpi"] = 100
+    plt.rcParams["font.size"] = 16
+    plt.rcParams["font.family"] = "sans-serif"
+    plt.rcParams["font.sans-serif"] = ["Liberation Sans"]
+    plt.rcParams["font.cursive"] = ["Liberation Sans"]
+    plt.rcParams["mathtext.fontset"] = "custom"
+    plt.rcParams["image.cmap"] = "inferno"

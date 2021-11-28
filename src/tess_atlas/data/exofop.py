300dbc054f5233e26254ca00487c08c6f01d9798
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-11-28 17:04:00 +1100

    move exofop queries to new module (#153)

---
+++ src/tess_atlas/data/exofop.py
@@ -0,0 +1,69 @@
+import logging
+import os
+from typing import Dict, List, Optional
+
+import pandas as pd
+from arviz import InferenceData
+from IPython.display import HTML, display
+
+from tess_atlas.utils import NOTEBOOK_LOGGER_NAME
+
+from .data_object import DataObject
+from .inference_data_tools import (
+    get_idata_fname,
+    load_inference_data,
+    save_inference_data,
+)
+from .lightcurve_data import LightCurveData
+from .planet_candidate import PlanetCandidate
+from .stellar_data import StellarData
+
+logger = logging.getLogger(NOTEBOOK_LOGGER_NAME)
+
+EXOFOP = "https://exofop.ipac.caltech.edu/tess/"
+TIC_DATASOURCE = EXOFOP + "download_toi.php?sort=toi&output=csv"
+TIC_SEARCH = EXOFOP + "target.php?id={tic_id}"
+
+DIR = os.path.dirname(__file__)
+
+
+def get_tic_database():
+    # if we have a cached database file
+    cached_file = os.path.join(DIR, "cached_tic_database.csv")
+    if os.path.isfile(cached_file):
+        db = pd.read_csv(cached_file)
+    else:
+        # go online to grab database and cache
+        db = pd.read_csv(TIC_DATASOURCE)
+        db.to_csv(cached_file)
+    return db
+
+
+def get_tic_id_for_toi(toi_number: int) -> int:
+    tic_db = get_tic_database()
+    toi = tic_db[tic_db["TOI"] == toi_number + 0.01].iloc[0]
+    return int(toi["TIC ID"])
+
+
+def get_tic_data_from_database(toi_numbers: List[int]) -> pd.DataFrame:
+    """Get rows of about a TIC  from ExoFOP associated with a TOI target.
+    :param int toi_numbers: The list TOI number for which the TIC data is required
+    :return: Dataframe with all TOIs for the TIC which contains TOI {toi_id}
+    :rtype: pd.DataFrame
+    """
+    tic_db = get_tic_database()
+    tics = [get_tic_id_for_toi(toi) for toi in toi_numbers]
+    dfs = [tic_db[tic_db["TIC ID"] == tic].sort_values("TOI") for tic in tics]
+    tois_for_tic = pd.concat(dfs)
+    if len(tois_for_tic) < 1:
+        raise ValueError(f"TOI data for TICs-{tics} does not exist.")
+    return tois_for_tic
+
+
+def get_tic_url(tic_id):
+    return TIC_SEARCH.format(tic_id=tic_id)
+
+
+def get_toi_list():
+    database = get_tic_database()
+    return list(set(database["TOI"].values))

400c952c8a78c93189f87d298f042e3c38f6ab69
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-12-16 16:17:20 +1100

    add tar creation at the end of book-build

--- src/tess_atlas/webbuilder/build_pages.py
+++ src/tess_atlas/webbuilder/build_pages.py
@@ -15,22 +15,21 @@ from tess_atlas.webbuilder.make_tois_homepage import make_menu_page
 dist_log.set_verbosity(dist_log.INFO)
 dist_log.set_threshold(dist_log.INFO)
 
-DEFAULT_TEMPLATES_DIR = f"{os.path.dirname(__file__)}/../../../docs/"
+TEMPLATES_DIR = f"{os.path.dirname(__file__)}/../../../docs/"
 NOTEBOOKS_DIR = "content/toi_notebooks"
 MENU_PAGE = "content/toi_fits.md"
 
 
-def make_book(outdir, notebooks_dir, templates_dir=DEFAULT_TEMPLATES_DIR):
+def make_book(outdir: str, notebooks_dir: str, rebuild: bool):
     outdir_present = os.path.isdir(outdir)
     new_notebook_dir = os.path.join(outdir, NOTEBOOKS_DIR)
 
     if outdir_present:
         print(f"Outdir ({os.path.abspath(outdir)}) exists")
-        input_var = input("press [r] to rebuild, [u] to update [r/u]: ")
-        if input_var == "r":
+        if rebuild:
             print(f"Website being built at {outdir}")
             os.makedirs(outdir, exist_ok=True)
-            copy_tree(templates_dir, outdir)  # copy templates to outdir
+            copy_tree(TEMPLATES_DIR, outdir)  # copy templates to outdir
             copy_tree(
                 notebooks_dir, new_notebook_dir
             )  # copy notebooks to new outdir
@@ -43,6 +42,13 @@ def make_book(outdir, notebooks_dir, templates_dir=DEFAULT_TEMPLATES_DIR):
     )
     subprocess.run(f"jupyter-book build {outdir}", shell=True, check=True)
 
+    print(f"TARing contents")
+    subprocess.run(
+        f"tar -czf {os.path.dirname(outdir)}.tar {outdir}",
+        shell=True,
+        check=True,
+    )
+
 
 def get_cli_args():
     """Get the TOI number from the CLI and return it"""
@@ -52,20 +58,17 @@ def get_cli_args():
         "notebooks_dir", type=str, help="Directory with analysed notebooks"
     )
     parser.add_argument(
-        "templates_dir",
-        default=DEFAULT_TEMPLATES_DIR,
-        type=str,
-        help="Directory with templates",
+        "--rebuild",
+        action="store_true",  # False by default
+        help="Rebuild from scratch (even if some webpages exist).",
     )
     args = parser.parse_args()
-    return args.web_outdir, args.notebooks_dir, args.templates_dir
+    return args.web_outdir, args.notebooks_dir, args.rebuild
 
 
 def main():
-    outdir, notebooks_dir, templates_dir = get_cli_args()
-    make_book(
-        outdir=outdir, notebooks_dir=notebooks_dir, templates_dir=templates_dir
-    )
+    outdir, notebooks_dir, rebuild = get_cli_args()
+    make_book(outdir=outdir, notebooks_dir=notebooks_dir, rebuild=rebuild)
 
 
 if __name__ == "__main__":

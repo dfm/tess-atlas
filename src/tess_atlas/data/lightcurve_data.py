35e84f24195fc0ac30b55195d7335734a4065fe9
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-07-11 13:44:23 -0400

    cadence fix

--- src/tess_atlas/data/lightcurve_data.py
+++ src/tess_atlas/data/lightcurve_data.py
@@ -72,7 +72,7 @@ class LightCurveData(DataObject):
     @property
     def cadence(self):
         """How often TESS photometric observations are stored."""
-        return np.min(np.diff(self.lc.time))
+        return np.min(np.diff(self.time))
 
     def to_dict(self):
         return dict(time=self.time, flux=self.flux, flux_err=self.flux_err)

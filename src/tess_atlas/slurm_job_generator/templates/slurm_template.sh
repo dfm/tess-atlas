9107201a4bb584f8d66b23c5bdc4364c512b5d16
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-10-10 16:44:18 +1100

    add 'partition' to slurm file (#251)
    * add slurm partition

--- src/tess_atlas/slurm_job_generator/templates/slurm_template.sh
+++ src/tess_atlas/slurm_job_generator/templates/slurm_template.sh
@@ -7,6 +7,8 @@
 #SBATCH --time={{time}}
 #SBATCH --mem={{mem}}
 #SBATCH --cpus-per-task={{cpu_per_task}}
+{% if partition!="" -%}      #SBATCH --partition={{partition}}
+{% endif %}
 {% if array_job=="True" -%}      #SBATCH --array=0-{{array_end}}
 {% endif %}
 module load {{module_loads}}

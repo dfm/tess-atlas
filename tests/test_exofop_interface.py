13f3b782600b3080e85dcb5156cf2c1bc77366bb
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-10-03 20:52:40 +1300

    run tests

--- tests/test_exofop_interface.py
+++ tests/test_exofop_interface.py
@@ -20,34 +20,3 @@ def test_get_toi_numbers_for_different_categories():
     toi_numbers = EXOFOP_DATA.get_categorised_toi_lists()
     assert isinstance(toi_numbers.single_transit, List)
     assert len(toi_numbers.single_transit) > 0
-
-
-@pytest.fixture
-def mock_lighcurve_check(monkeypatch):
-    """Mock tess_atlas.data.exofop.exofp_database._lightcurve_available(tic: int)"""
-
-    def mock_lighcurve_check(tic: int):
-        return np.nan
-
-    monkeypatch.setattr(
-        "tess_atlas.data.exofop.exofop_database._lightcurve_available",
-        mock_lighcurve_check,
-    )
-
-
-def test_update(tmp_path, mock_lighcurve_check, monkeypatch):
-    logging.getLogger(LOGGER_NAME).setLevel(logging.DEBUG)
-
-    # mock the TIC_DATASOURCE --> TIC_CACHE
-    N_LINES = 10
-    tmp_csv = str(tmp_path / "cached_tic_database.csv")
-    with open(exofop_database.TIC_CACHE) as old, open(tmp_csv, "w") as new:
-        lines = old.readlines()
-        new.writelines(lines[: 1 + N_LINES])  # write header + N_LINES
-
-    monkeypatch.setattr(exofop_database, "TIC_DATASOURCE", tmp_csv)
-    monkeypatch.setattr(exofop_database, "TIC_CACHE", tmp_csv)
-
-    fname = str(tmp_path / "test.csv")
-    exo_db = ExofopDatabase(cache_fname=tmp_csv)
-    exo_db.update(save_name=fname)
